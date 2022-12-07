78a29395eeb6ff1f4cbb2deadb2e0641b9b6e173
agolo <agolo@DESKTOP-VAVTTP2>
2022-12-07 14:05:12 +0200

    Add project files.

---
+++ Plugins.DataStore.InMemory/CategoryInMemoryRepository.cs
@@ -0,0 +1,24 @@
+﻿using CoreBusiness;
+using UseCases.DaraStorePluginInterfaces;
+
+namespace Plugins.DataStore.InMemory
+{
+    public class CategoryInMemoryRepository : ICategoryRepository
+    {
+        private List<Category> categories;
+        public  CategoryInMemoryRepository()
+        {
+            // Add some default categories 
+            categories = new List<Category>()
+            {
+                new Category{CategoryId = 1, Name = "Beverage", Description = "Beverage"},
+                new Category{CategoryId = 2, Name = "Bakery", Description = "Bakery"},
+                new Category{CategoryId = 3, Name = "Meat", Description = "Meat"},
+            };
+        }
+        public IEnumerable<Category> GetCategories()
+        {
+            return categories;
+        }
+    }
+}
