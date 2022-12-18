76dc3337ee270c9d14b7086a2cfff3e3bf92a226
agoloveichuk@gmail.com <agoloveichuk@gmail.com>
2022-12-18 20:02:00 +0200

    Cashier Console Use Case

---
+++ UseCases/SellProductUseCase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using UseCases.DaraStorePluginInterfaces;
+using UseCases.ProductsUseCase;
+
+namespace UseCases
+{
+    public class SellProductUseCase : ISellProductUseCase
+    {
+        private readonly IProductRepository productRepository;
+
+        public SellProductUseCase(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        public void Execute(int productId, int qtyToSell)
+        {
+            var product = productRepository.GetProductById(productId);
+            if (product == null) return;
+
+            product.Quantity -= qtyToSell;
+            productRepository.UpdateProduct(product);
+        }
+    }
+}
